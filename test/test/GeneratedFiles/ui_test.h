26b8b4154f0640ffe60f9c3bdaeb587586e1202d
Paweł Rother <pawerot276@student.polsl.pl>
2017-01-15 21:16:00 +0100

    test

---
+++ test/test/GeneratedFiles/ui_test.h
@@ -0,0 +1,76 @@
+/********************************************************************************
+** Form generated from reading UI file 'test.ui'
+**
+** Created by: Qt User Interface Compiler version 5.8.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_TEST_H
+#define UI_TEST_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_testClass
+{
+public:
+    QWidget *centralWidget;
+    QPushButton *pushButton;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *testClass)
+    {
+        if (testClass->objectName().isEmpty())
+            testClass->setObjectName(QStringLiteral("testClass"));
+        testClass->resize(600, 400);
+        centralWidget = new QWidget(testClass);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        pushButton = new QPushButton(centralWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+        pushButton->setGeometry(QRect(80, 90, 75, 23));
+        testClass->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(testClass);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 600, 21));
+        testClass->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(testClass);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        testClass->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(testClass);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        testClass->setStatusBar(statusBar);
+
+        retranslateUi(testClass);
+
+        QMetaObject::connectSlotsByName(testClass);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *testClass)
+    {
+        testClass->setWindowTitle(QApplication::translate("testClass", "test", Q_NULLPTR));
+        pushButton->setText(QApplication::translate("testClass", "Kliknij", Q_NULLPTR));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class testClass: public Ui_testClass {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_TEST_H
